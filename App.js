91a2adef159a0dd33fdab5f0bf6c7072a011ebdd
Faith Makulah Swen <swenfaithm@gmail.com>
2024-02-15 22:35:40 +0200

    mm

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View,Text  } from 'react-native';
+import { SignIn } from './screen/SignIn';
 
-export default function App() {
+import { Login } from './LoginRegister/Login';
+import { Register } from './LoginRegister/Register';
+// import Sign from './Signin';
+
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {/* <Text>food</Text> */}
+      {/* <SignIn/> */}
+      {/* <Todo /> */}
+      {/* <Login/> */}
+      <Register/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
